a9f4d59674631b04edec49b101c7e4972b5177cd
CODe5753 <code.57x53@gmail.com>
2022-09-22 18:52:18 +0900

    feat: Modify service layer

--- src/main/java/com/example/demo/taskmanager/controller/TaskManagerController.java
+++ src/main/java/com/example/demo/taskmanager/controller/TaskManagerController.java
@@ -1,11 +1,15 @@
 package com.example.demo.taskmanager.controller;
 
 import com.example.demo.common.dto.ResponseDto;
+import com.example.demo.taskmanager.dto.JobDto;
 import com.example.demo.taskmanager.service.TaskManagerService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,9 +19,9 @@ public class TaskManagerController {
 
     private final TaskManagerService taskManagerService;
 
-    @PostMapping("/job")
-    public ResponseDto<?> pushJob(@RequestBody String provider, @RequestBody String description) {
-        var response = taskManagerService.runJob(provider, description);
+    @GetMapping("/job")
+    public ResponseDto<?> getJobStatus(@RequestParam String jobId) {
+        var response = taskManagerService.getJob(jobId);
         return ResponseDto.success(response);
     }
 }
