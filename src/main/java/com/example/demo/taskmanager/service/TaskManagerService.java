6c33eec74e7f2c0b788871e3313b6cd64a4e2d13
CODe5753 <code.57x53@gmail.com>
2022-09-22 18:51:47 +0900

    feat: Implementation of logic that reflects state

--- src/main/java/com/example/demo/taskmanager/service/TaskManagerService.java
+++ src/main/java/com/example/demo/taskmanager/service/TaskManagerService.java
@@ -1,42 +1,38 @@
 package com.example.demo.taskmanager.service;
 
 import com.example.demo.taskmanager.dto.JobDto;
-import com.example.demo.taskmanager.dto.JobStatus;
 import com.example.demo.taskmanager.entity.Job;
 import com.example.demo.taskmanager.entity.JobLog;
 import com.example.demo.taskmanager.repository.JobLogRepository;
 import com.example.demo.taskmanager.repository.JobRepository;
-import java.util.UUID;
 import lombok.RequiredArgsConstructor;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class TaskManagerService {
 
     private final JobRepository jobRepository;
     private final JobLogRepository jobLogRepository;
-    private final RabbitTemplate rabbitTemplate;
 
-    public JobDto runJob(String provider, String description) {
-        String uuid = UUID.randomUUID().toString();
-        String status = JobStatus.READY.getStatus();
+    public JobDto getJob(String jobId) {
+        JobDto jobDto = new JobDto(jobRepository.findById(jobId).get());
+        return jobDto;
+    }
 
-        JobDto job = JobDto.builder()
-            .provider(provider)
-            .jobId(uuid)
-            .status(status)
-            .description(description)
-            .build();
+    @Transactional
+    public void updateJobStatus(JobDto jobDto) {
+        log.info("update => {}", jobDto.toString());
 
-        Job jobEntity = job.toJobEntity(uuid);
-        JobLog jobLogEntity = job.toJobLogEntity(jobEntity);
-        jobRepository.save(jobEntity);
-        jobLogRepository.save(jobLogEntity);
+        Job job = jobDto.toJobEntity();
+        jobRepository.save(job);
 
-        /* todo: rabbitTemplate을 이용한 이벤트 처리 */
+        JobLog jobLog = jobDto.toJobLogEntity(job);
+        jobLogRepository.save(jobLog);
 
-        return job;
+        log.info("update done!");
     }
 }
